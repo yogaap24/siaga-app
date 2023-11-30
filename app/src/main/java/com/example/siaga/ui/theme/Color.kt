2d2b6c76d108974f545d1dc8aa54553d091aa601
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2023-11-30 14:39:14 +0700

    fix customOutlineField

---
+++ app/src/main/java/com/example/siaga/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.siaga.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Red20 = Color(0xFFE85646)
+val White10 = Color(0xFFF0EEEE)
+val White20 = Color(0xFFCFCCCC)
+val Gray30 = Color(0xFF6E6666)
